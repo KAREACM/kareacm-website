559bb6737fa02137ce30da6431accb7d7abd9b35
kannapranavi15 <kannapranavi15@gmail.com>
2024-09-13 14:09:55 +0530

    Your commit message

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,4 +1,4 @@
-/** 
+/**
  * FileName - data.js
  * FileType - .js (JavaScript file)
  * Lines - 821
@@ -9,134 +9,859 @@
 
 // Executive Team List
 export const ExecList = [
+  {
+    id: 0,
+    name: 'BHANU PRATHAP SHOURYA',
+    position: 'Chairperson',
+    image: prathap_image,
+    linkedin: "#",
+    github: "#",
+    instagram: "#",
+  },
+  {
+    id: 1,
+    name: 'B LOKESH KUMAR',
+    position: 'Vice-Chairperson',
+    image: lokesh_image,
+    linkedin: "#",
+    github: "#",
+    instagram: "#",
+  },
+  {
+    id: 2,
+    name: 'JASWANTH S',
+    position: 'Secretary',
+    image: jaswanth_image,
+    linkedin: "#",
+    github: "#",
+    instagram: "#",
+  },
+  {
+    id: 3,
+    name: 'LIGORIS DEVANANDRAJ',
+    position: 'Treasurer',
+    image: ligoris_image,
+    linkedin: "#",
+    github: "#",
+    instagram: "#",
+  },
+  {
+    id: 4,
+    name: 'T JEYROUS JOSHUA',
+    position: 'Membership-chair',
+    image: joshua_image,
+    linkedin: "#",
+    github: "#",
+    instagram: "#",
+  },
+  {
+    id: 5,
+    name: 'YASHWANTH A',
+    position: 'Webmaster',
+    image: yashwanth_image,
+    linkedin: "#",
+    github: "#",
+    instagram: "#",
+  },
+  {
+    id: 6,
+    name: 'BHAVAN SAI',
+    position: 'Event-coordinator',
+    image: Bhavan_image,
+    linkedin: "#",
+    github: "#",
+    instagram: "#",
+  },
+];
+
+// code sprint
+import cs from "src\assets\Images\code_sprint\img.jpeg";
+export const codex =[
+  {
+    id:0,
+    image: cs,
+  },
+]
+
+// conversation on ai ml
+
+import cmi1 from "src\assets\Images\conversation_on_ml_sw_hw\img1.jpeg";
+import cmi2 from "src\assets\Images\conversation_on_ml_sw_hw\img2.jpeg";
+export const aiml =[
+  {
+    id:0,
+    image:cmi1,
+  },
+  {
+    id:1,
+    image:cmi2,
+  },
+]
+
+//cook the code
+import ctc1 from "src\assets\Images\cook_the_code\img1.jpg";
+import ctc2 from "src\assets\Images\cook_the_code\img2.jpg";
+import ctc3 from "src\assets\Images\cook_the_code\img3.jpg";
+import ctc4 from "src\assets\Images\cook_the_code\img4.jpg";
+import ctc5 from "src\assets\Images\cook_the_code\img5.jpeg";
+import ctc6 from "src\assets\Images\cook_the_code\img6.jpeg";
+import ctc7 from "src\assets\Images\cook_the_code\img7.jpeg";
+import ctc8 from "src\assets\Images\cook_the_code\img8.jpeg";
+
+export const cookthecode =[
+  {
+    id:0,
+    image:ctc1,
+  },
+  {
+    id:1,
+    image:ctc2,
+  },
+  {
+    id:2,
+    image:ctc3,
+  },
+  {
+    id:3,
+    image:ctc4,
+  },
+  {
+    id:4,
+    image:ctc5,
+  },
+  {
+    id:5,
+    image:ctc6,
+  },
+  {
+    id:6,
+    image:ctc7,
+  },
+  {
+    id:7,
+    image:ctc8,
+  },
+]
+
+// es by prashanth
+import es1 from "src\assets\Images\es_by_prashanth\img1.jpeg";
+import es2 from "src\assets\Images\es_by_prashanth\img2.jpeg";
+import es3 from "src\assets\Images\es_by_prashanth\img3.jpeg";
+import es4 from "src\assets\Images\es_by_prashanth\img4.jpeg";
+import es5 from "src\assets\Images\es_by_prashanth\img5.jpeg";
+
+export const prashanth =[
+  {
+    id:0,
+    image:es1,
+  },
+  {
+    id:1,
+    image:es2,
+  },
+  {
+    id:2,
+    image:es3,
+  },
+  {
+    id:3,
+    image:es4,
+  },
+  {
+    id:4,
+    image:es5,
+  },
+  {
+    id:5,
+    image:es6,
+  },
+  {
+    id:6,
+    image:es7,
+  },
+  {
+    id:7,
+    image:es8,
+  },
+]
+// esp blast
+import esp1 from "src\assets\Images\esp_blast\img1.jpeg";
+import esp2 from "src\assets\Images\esp_blast\img2.jpg";
+import esp3 from "src\assets\Images\esp_blast\img3.jpg";
+import esp4 from "src\assets\Images\esp_blast\img4.jpg";
+import esp5 from "src\assets\Images\esp_blast\img5.jpg";
+import esp6 from "src\assets\Images\esp_blast\img6.jpg";
+
+export const espblast =[
+  {
+    id:0,
+    image:esp1,
+  },
+  {
+    id:1,
+    image:esp2,
+  },
+  {
+    id:2,
+    image:esp3,
+  },
+  {
+    id:3,
+    image:esp4,
+  },
+  {
+    id:4,
+    image:esp5,
+  },
+  {
+    id:5,
+    image:esp6,
+  },
+
+]
+
+//esp in analytics
+
+import espa1 from "src\assets\Images\esp_in_analytics\img1.jpeg";
+import espa2 from "src\assets\Images\esp_in_analytics\img2.jpeg";
+export const esp_in_analytics =[
+  {
+    id:0,
+    image:espa1,
+  },
+  {
+    id:1,
+    image:espa2,
+  },
+]
+
+//esp on ioe
+
+import ioe1 from "src\assets\Images\esp_on_ioe\img1.jpeg";
+import ioe2 from "src\assets\Images\esp_on_ioe\img2.jpg";
+import ioe3 from "src\assets\Images\esp_on_ioe\img3.jpg";
+import ioe4 from "src\assets\Images\esp_on_ioe\img4.jpg";
+import ioe5 from "src\assets\Images\esp_on_ioe\img5.jpg";
+
+
+export const esp_on_ioe =[
+  {
+    id:0,
+    image:ioe1,
+  },
+  {
+    id:1,
+    image:ioe2,
+  },
+  {
+    id:2,
+    image:ioe3,
+  },
+  {
+    id:3,
+    image:ioe4,
+  },
+  {
+    id:4,
+    image:ioe5,
+  },
+]
+
+//esp on security
+import esps1 from "src\assets\Images\esp_on_security_trust\img1.jpeg";
+import esps2 from "src\assets\Images\esp_on_security_trust\img2.jpg";
+import esps3 from "src\assets\Images\esp_on_security_trust\img3.jpg";
+import esps4 from "src\assets\Images\esp_on_security_trust\img4.jpg";
+import esps5 from "src\assets\Images\esp_on_security_trust\img5.jpg";
+import esps6 from "src\assets\Images\esp_on_security_trust\img6.jpg";
+import esps7 from "src\assets\Images\esp_on_security_trust\img7.jpg";
+import esps8 from "src\assets\Images\esp_on_security_trust\img8.jpg";
+import esps9 from "src\assets\Images\esp_on_security_trust\img9.jpg";
+import esps10 from "src\assets\Images\esp_on_security_trust\img10.jpg";
+
+export const esp_on_security =[
+  {
+    id:0,
+    image:esps1,
+  },
+  {
+    id:1,
+    image:esps2,
+  },
+  {
+    id:2,
+    image:esps3,
+  },
+  {
+    id:3,
+    image:esps4,
+  },
+  {
+    id:4,
+    image:esps5,
+  },
+  {
+    id:5,
+    image:esps6,
+  },
+  {
+    id:6,
+    image:esps7,
+  },
+  {
+    id:7,
+    image:esps8,
+  },
+  {
+    id:8,
+    image:esps9,
+  },
+  {
+    id:9,
+    image:esps10,
+  },
+  
+]
+
+// fun with algorithms
+import fwa1 from "src\assets\Images\fun_with_algorithms\img1.jpeg";
+import fwa2 from "src\assets\Images\fun_with_algorithms\img2.jpeg";
+
+export const fun_with_algorithms =[
+  {
+    id:0,
+    image:fwa1,
+  },
+  {
+    id:1,
+    image:fwa2,
+  },
+
+]
+
+//hackare
+import hac1 from "src\assets\Images\HACKARE\img1.jpeg";
+import hac2 from "src\assets\Images\HACKARE\img2.jpeg";
+import hac3 from "src\assets\Images\HACKARE\img3.jpeg";
+import hac4 from "src\assets\Images\HACKARE\img4.jpg";
+import hac5 from "src\assets\Images\HACKARE\img5.jpeg";
+import hac6 from "src\assets\Images\HACKARE\img6.jpeg";
+
+export const hackare=[
+  {
+    id:0,
+    image:hac1,
+  },
+  {
+    id:1,
+    image:hac2,
+  },
+  {
+    id:2,
+    image:hac3,
+  },
+  {
+    id:3,
+    image:hac4,
+  },
+  {
+    id:4,
+    image:hac5,
+  },
+  {
+    id:5,
+    image:hac6,
+  },
+]
+
+//hackodyssesy
+import hod1 from "src\assets\Images\hack_odyssey\img1.jpeg";
+import hod2 from "src\assets\Images\hack_odyssey\img2.jpeg";
+import hod3 from "src\assets\Images\hack_odyssey\img3.jpeg";
+import hod4 from "src\assets\Images\hack_odyssey\img4.jpeg";
+import hod5 from "src\assets\Images\hack_odyssey\img5.jpeg";
+import hod6 from "src\assets\Images\hack_odyssey\img6.jpeg";
+import hod7 from "src\assets\Images\hack_odyssey\img7.jpeg";
+
+export const hack_odyssey=[
+  {
+    id:0,
+    image:hod1,
+  },
+  {
+    id:1,
+    image:hod2,
+  },
+  {
+    id:2,
+    image:hod3,
+  },
+  {
+    id:3,
+    image:hod4,
+  },
+  {
+    id:4,
+    image:hod5,
+  },
+  {
+    id:5,
+    image:hod6,
+  },
+  {
+    id:6,
+    image:hod7,
+  },
+]
+
+//hour of code
+
+import hofc1 from "src\assets\Images\hour_of_code\img1.jpeg";
+import hofc2 from "src\assets\Images\hour_of_code\img2.jpeg";
+
+export const hour_of_codes=[
+  {
+    id:0,
+    image:hofc1,
+  },
+  {
+    id:1,
+    image:hofc2,
+  },
+]
+
+//idea sparks
+
+//round1
+
+import is1 from "src\assets\Images\idea sparks\round_1\img1.jpeg"
+import is2 from "src\assets\Images\idea sparks\round_1\img2.jpeg"
+import is3 from "src\assets\Images\idea sparks\round_1\img3.DNG"
+import is4 from "src\assets\Images\idea sparks\round_1\img4.png"
+import is5 from "src\assets\Images\idea sparks\round_1\img5.png"
+import is6 from "src\assets\Images\idea sparks\round_1\img6.png"
+import is7 from "src\assets\Images\idea sparks\round_1\img7.png"
+
+export const idea_spark_1=[
+  {
+    id:0,
+    image:is1,
+  },
+  {
+    id:1,
+    image:is2,
+  },
+  {
+    id:2,
+    image:is3,
+  },
+  {
+    id:3,
+    image:is4,
+  },
+  {
+    id:4,
+    image:is5,
+  },
+  {
+    id:5,
+    image:is6,
+  },
+  {
+    id:6,
+    image:is7,
+  },
+]
+
+//round2
+import iss1 from "src\assets\Images\idea sparks\round_2\img1.jpeg"
+import iss2 from "src\assets\Images\idea sparks\round_2\img2.jpg"
+import iss3 from "src\assets\Images\idea sparks\round_2\img3.jpg"
+import iss4 from "src\assets\Images\idea sparks\round_2\img4.jpg"
+
+export const idea_sparks_2=[
+  {
+    id:0,
+    image:iss1,
+  },
+  {
+    id:1,
+    image:iss2,
+  },
+  {
+    id:2,
+    image:iss3,
+  },
+  {
+    id:3,
+    image:iss4,
+  },
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Gallery List
+export const GalleryList = [
+  {
+    img: EventImage1,
+    title: 'Opening Ceremony 2023',
+  },
+  {
+    img: EventImage2,
+    title: 'Keynote Speech by Industry Expert',
+  },
+  // Add more gallery images here
+];
+
+// Projects List
+export const ProjectsList = [
+  {
+    id: 'Project-1',
+    photo: AIProjectImage,
+    title: 'AI for Accessibility',
+    description: 'This project focuses on using artificial intelligence to assist individuals with disabilities by offering real-time accessibility solutions.',
+  },
+  {
+    id: 'Project-2',
+    photo: IoTProjectImage,
+    title: 'IoT Smart Home',
+    description: 'An IoT-based smart home system that allows users to control their home appliances remotely via mobile devices.',
+  },
+  // Add more projects here
+];
+
+// Faculty List
+export const FacultyList = {
+  id: 1,
+  facultymembers: 'faculty',
+  heads: [
     {
       id: 0,
-      name: 'John Doe',
-      position: 'Chairperson',
-      image: JohnDoeImage,
-      linkedin: "https://linkedin.com/in/johndoe",
-      github: "https://github.com/johndoe",
-      instagram: "https://instagram.com/johndoe",
+      photo: photos,
+      name: 'DR P DEEPA LAKSHMI',
+      post: 'DEAN/SOC',
     },
     {
       id: 1,
-      name: 'Jane Doe',
-      position: 'Vice-Chairperson',
-      image: JaneDoeImage,
-      linkedin: "https://linkedin.com/in/janedoe",
-      github: "https://github.com/janedoe",
-      instagram: "https://instagram.com/janedoe",
+      photo: photos,
+      name: 'DR N SURESH KUMAR',
+      post: 'HOD/CSE',
     },
-    // Add more executive team members
-  ];
-  
-  // Home Page Events List
-  export const EventsList = [
-    {
-      id: 'event-1',
-      name: 'Tech Conference 2023',
-      image: TechConferenceImage,
-      desc: 'The annual Tech Conference brings together over 1000 participants to explore the latest innovations in technology, focusing on AI, IoT, and cybersecurity.',
-    },
-    {
-      id: 'event-2',
-      name: 'Hackathon 2024',
-      image: HackathonImage,
-      desc: 'A 48-hour hackathon where participants push the boundaries of innovation in software development, collaborating to solve real-world problems.',
-    },
-    // Add more events
-  ];
-  
-  // Gallery List
-  export const GalleryList = [
-    {
-      img: EventImage1,
-      title: 'Opening Ceremony 2023',
-    },
-    {
-      img: EventImage2,
-      title: 'Keynote Speech by Industry Expert',
-    },
-    // Add more gallery images
-  ];
-  
-  // Projects List
-  export const ProjectsList = [
-    {
-      id: 'Project-1',
-      photo: AIProjectImage,
-      title: 'AI for Accessibility',
-      description: 'This project focuses on using artificial intelligence to assist individuals with disabilities by offering real-time accessibility solutions.',
-    },
-    {
-      id: 'Project-2',
-      photo: IoTProjectImage,
-      title: 'IoT Smart Home',
-      description: 'An IoT-based smart home system that allows users to control their home appliances remotely via mobile devices.',
-    },
-    // Add more projects
-  ];
-  
-  // Team Heads List
-  export const TeamHeadsList = [
-    // Community Managers
-    {
-      id: 0,
-      teamName: 'Community Managers',
-      heads: [
-        {
-          id: 0,
-          photo: Manager1Image,
-          name: 'Alice Smith',
-          post: 'Community Manager',
-        },
-        {
-          id: 1,
-          photo: Manager2Image,
-          name: 'Bob Johnson',
-          post: 'Community Manager',
-        },
-      ]
-    },
-    // Events Team
-    {
-      id: 1,
-      teamName: 'Events',
-      heads: [
-        {
-          id: 0,
-          photo: EventHead1Image,
-          name: 'Charlie Williams',
-          post: 'Head of Events',
-        },
-        {
-          id: 1,
-          photo: EventHead2Image,
-          name: 'Dana Brown',
-          post: 'Deputy Head of Events',
-        },
-        // Add more event team members
-      ]
-    },
-    // Finance Team
     {
       id: 2,
-      teamName: 'Finance',
+      photo: photos,
+      name: 'DR P NAGARAJ',
+      post: 'FACULTY SPONSOR',
+    },
+    {
+      id: 3,
+      photo: photos,
+      name: 'MRS S AMUTHA',
+      post: 'FACULTY CO-ORDINATOR',
+    },
+  ],
+};
+
+// Finance Team List
+export const FinanceTeam = {
+  id: 2,
+  teamName: 'Finance',
+  heads: [
+    {
+      id: 0,
+      photo: FinanceHead1Image,
+      name: 'SETHU SAI RAM',
+      post: 'Finance team member',
+    },
+    {
+      id: 1,
+      photo: FinanceHead2Image,
+      name: 'K AMARNATH',
+      post: 'Finance team member',
+    },
+    {
+      id: 2,
+      photo: FinanceHead3Image,
+      name: 'Y YASHWANTH',
+      post: 'Finance team member',
+    },
+    {
+      id: 3,
+      photo: FinanceHead4Image,
+      name: 'P SATHVIKA',
+      post: 'Finance team member',
+    },
+    {
+      id: 4,
+      photo: FinanceHead5Image,
+      name: 'K SOWMYA',
+      post: 'Finance team member',
+    },
+  ],
+};
+
+// Social Media Team List
+export const SocialMediaTeam = {
+  id: 3,
+  teamName: 'Social Media Team',
+  heads: [
+    {
+      id: 0,
+      photo: photos,
+      name: 'M SOWMYA',
+      post: 'Social media team member',
+    },
+    {
+      id: 1,
+      photo: photos,
+      name: 'C BAVESH',
+      post: 'Social media team member',
+    },
+    {
+      id: 2,
+      photo: photos,
+      name: 'M MARUTHI KUMAR',
+      post: 'Social media team member',
+    },
+    {
+      id: 3,
+      photo: photos,
+      name: 'SUJITHA EDARA',
+      post: 'Social media team member',
+    },
+    {
+      id: 4,
+      photo: photos,
+      name: 'Y VIJAYA KUMARI',
+      post: 'Social media team member',
+    },
+  ],
+};
+
+// Event Coordinators Team List
+export const EventCoordinatorsTeam = {
+  id: 4,
+  teamName: 'Event Coordinators Team',
+  heads: [
+    {
+      id: 0,
+      photo: photos,
+      name: 'K JAIDEEP',
+      post: 'Event coordinators team member',
+    },
+    {
+      id: 1,
+      photo: photos,
+      name: 'I SONIESH',
+      post: 'Event coordinators team member',
+    },
+    {
+      id: 2,
+      photo: photos,
+      name: 'J PAVITHRA',
+      post: 'Event coordinators team member',
+    },
+    {
+      id: 3,
+      photo: photos,
+      name: 'K TEJASREE',
+      post: 'Event coordinators team member',
+    },
+    {
+      id: 4,
+      photo: photos,
+      name: 'V INDHUMATHI',
+      post: 'Event coordinators team member',
+    },
+  ],
+};
+
+// Web Developers Team List
+export const WebDevelopersTeam = {
+  id: 5,
+  teamName: 'Web Developers Team',
+  heads: [
+    {
+      id: 0,
+      photo: photos,
+      name: 'C KARTHIK VARMA',
+      post: 'Web developer team member',
+    },
+    {
+      id: 1,
+      photo: photos,
+      name: 'V S HARSHAD',
+      post: 'Web developer team member',
+    },
+    {
+      id: 2,
+      photo: photos,
+      name: 'ATHITHYA RAMA',
+      post: 'Web developer team member',
+    },
+    {
+      id: 3,
+      photo: photos,
+      name: 'B MAHA SRI',
+      post: 'Web developer team member',
+    },
+    {
+      id: 4,
+      photo: photos,
+      name: 'PRANAVI SRINIVAS',
+      post: 'Web developer team member',
+    },
+  ],
+};
+
+// Volunteers Team List
+export const VolunteersTeam = {
+  id: 6,
+  teamName: 'Volunteers Team',
+  heads: [
+    {
+      id: 0,
+      photo: photos,
+      name: 'J SAATHWIK REDDY',
+      post: 'Volunteer team member',
+    },
+    {
+      id: 1,
+      photo: photos,
+      name: 'M TANUJA',
+      post: 'Volunteer team member',
+    },
+    {
+      id: 2,
+      photo: photos,
+      name: 'M VANAJA',
+      post: 'Volunteer team member',
+    },
+  ],
+};
+   // INNOVATION TEAM 
+   export const innovationteam = {
+  
+    id: 7,
+    teamName: 'INNOVATION TEAM',
+    heads: [
+      {
+        id: 0,
+        photo: photos,
+        name: 'ISSAC PREETHAM',
+        post: 'INNOVATION TEAM MEMBER',
+      },
+      {
+      id: 1,
+      photo: photos,
+      name: 'CHATURA P',
+      post: 'INNOVATION TEAM MEMBER',
+      },
+      {
+        id: 2,
+        photo: photos,
+        name: 'BHARGHAV R',
+        post: 'INNOVATION TEAM MEMBER',
+      },
+      {
+        id: 3,
+        photo: photos,
+        name: 'GIRIJA SAI SIRI',
+        post: 'INNOVATION TEAM MEMBER TEAM MEMBER',
+      },
+      {
+        id: 4,
+        photo: photos,
+        name: 'Y MEENAKSHI',
+        post: 'INNOVATION TEAM MEMBER',
+      },
+      {
+        id: 5,
+        photo: photos,
+        name: 'KARNIKASRI',
+        post: 'INNOVATION TEAM MEMBER',
+      },
+      {
+        id: 6,
+        photo: photos,
+        name: 'Y SAI SREEJA',
+        post: 'INNOVATION TEAM MEMBER',
+      },
+      {
+        id: 7,
+        photo: photos,
+        name: 'S HARSHITHA',
+        post: 'INNOVATION TEAM MEMBER',
+      },
+     
+    ]
+
+  };
+  // GRAPHICS TEAM
+
+     export const graphics = {
+    id: 8,
+     teamName: 'GRAPHICS TEAM',
       heads: [
         {
-          id: 0,
-          photo: FinanceHead1Image,
-          name: 'Edward Green',
-          post: 'Head of Finance',
+         id: 0,
+         photo: photos,
+         name: 'GEETHESHWAR G',
+         post: 'GRAPHICS TEAM MEMBER',
         },
-        {
-          id: 1,
-          photo: FinanceHead2Image,
-          name: 'Fiona Blue',
-          post: 'Deputy Head of Finance',
+        { id: 1,
+          photo: photos,
+          name: 'P HARSHA PAVAN',
+          post: 'GRAPHICS TEAM MEMBER',
         },
+        { 
+          id: 2,
+          photo: photos,
+          name: 'N LAKSHMIKAR REDDY',
+          post: 'GRAPHICS TEAM MEMBER',
+        },
+
       ]
-    },
-    // Add more teams (e.g., marketing, technical, etc.)
-  ];
-  
+
+    };
+    //lens and edit team
+
+       export const lensandedit = {
+      id: 9,
+       teamName: 'LENS AND EDIT TEAM',
+        heads: [
+          {
+           id: 0,
+           photo: photos,
+           name: 'G DEVA VINAYAGAM',
+           post: 'LENS AND EDIT TEAM MEMBER',
+          },
+          {
+            id: 1,
+            photo: photos,
+            name: 'D CHETHAN',
+            post: 'LENS AND EDIT TEAM MEMBER',
+          },
+
+
+        ]
+
+      };
