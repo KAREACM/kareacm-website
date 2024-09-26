2f59adddbb7d5604ac001994595421773d323e54
imvsharshad <imvsharshad@gmail.com>
2024-09-26 18:25:38 +0530

    Added Faculties page

---
+++ src/new_design_components/Homepage/Faculty Coordinators/Faculties.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './../Faculty Coordinators/Faculties.css'
+
+function Faculties() {
+    return (
+        <div>
+            <div className="facuties-container">
+                <div className="faculty-coordinators-heading">
+                    Faculty Coordinators
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Faculties
