f46b8167450f85e6dce4ef1e16809c2eb2a9dffc
imvsharshad <imvsharshad@gmail.com>
2024-10-08 22:13:06 +0530

    added hover effects

--- src/new_design_components/Eventspage/ACMevents.jsx
+++ src/new_design_components/Eventspage/ACMevents.jsx
@@ -1,43 +1,43 @@
 import React from 'react';
-import { 
-  codex, 
-  aiml, 
-  cookthecode, 
-  prashanth, 
-  espblast, 
-  shouryaRoy, 
-  summerBoost,
-  esp_in_analytics,
-  esp_on_ioe,
-  esp_on_security,
-  fun_with_algorithms,
-  hackare,
-  hack_odyssey,
-  hour_of_codes,
-  idea_spark_1,
-  idea_sparks_2
-} from './../../constants/data.js'; 
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import './ACMEvents.css';  // CSS file import
+// import { 
+//   codex, 
+//   aiml, 
+//   cookthecode, 
+//   prashanth, 
+//   espblast, 
+//   shouryaRoy, 
+//   summerBoost,
+//   esp_in_analytics,
+//   esp_on_ioe,
+//   esp_on_security,
+//   fun_with_algorithms,
+//   hackare,
+//   hack_odyssey,
+//   hour_of_codes,
+//   idea_spark_1,
+//   idea_sparks_2
+// } from './../../constants/data.js'; 
+// import { Carousel } from 'react-responsive-carousel';
+// import 'react-responsive-carousel/lib/styles/carousel.min.css';
+// import './ACMEvents.css';  // CSS file import
 
 function ACMevents() {
-  const renderCarousel = (eventData, eventName) => (
-    <div className="eventSection">
-      <h2>{eventName}</h2>
-      <Carousel showThumbs={false} infiniteLoop autoPlay>
-        {eventData.map((event) => (
-          <div key={event.id} className="carouselItem">
-            <img src={event.image} alt={`${eventName} ${event.id}`} />
-          </div>
-        ))}
-      </Carousel>
-    </div>
-  );
+  // const renderCarousel = (eventData, eventName) => (
+  //   <div className="eventSection">
+  //     <h2>{eventName}</h2>
+  //     <Carousel showThumbs={false} infiniteLoop autoPlay>
+  //       {eventData.map((event) => (
+  //         <div key={event.id} className="carouselItem">
+  //           <img src={event.image} alt={`${eventName} ${event.id}`} />
+  //         </div>
+  //       ))}
+  //     </Carousel>
+  //   </div>
+  // );
 
   return (
     <>
-      <div className="container">
+      {/* <div className="container">
         <h1>Previous events | Upcoming events</h1>
 
         <div className="eventsWrapper">
@@ -60,7 +60,7 @@ function ACMevents() {
           {renderCarousel(idea_spark_1, 'Idea Sparks - Round 1')}
           {renderCarousel(idea_sparks_2, 'Idea Sparks - Round 2')}
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
--- src/new_design_components/Homepage/Faculty Coordinators/Faculties.jsx
+++ src/new_design_components/Homepage/Faculty Coordinators/Faculties.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './../Faculty Coordinators/Faculties.css'
+import React from 'react';
+import './../Faculty Coordinators/Faculties.css';
 import image1 from './../../../assets/Images/Shrouya_Roy/Img1.jpeg';
 import image2 from './../../../assets/Images/Shrouya_Roy/Img2.jpeg';
 import image3 from './../../../assets/Images/Shrouya_Roy/Img3.jpeg';
@@ -7,35 +7,37 @@ import image4 from './../../../assets/Images/Shrouya_Roy/Img4.jpeg';
 import Officebarriers from '../Office Barriers/Officebarriers';
 
 function Faculties() {
-    const faculty_coordinators = 
-    [
+    const faculty_coordinators = [
         {
-            id : 1,
-            name : "Image1",
-            position : "Faculty Coordinator",
-            image : image1
-
+            id: 1,
+            name: "Shrouya Roy",
+            position: "Faculty Coordinator",
+            image: image1,
+            linkedin: "https://www.linkedin.com/in/shrouya-roy/"
         },
         {
-            id : 2,
-            name : "Image2",
-            position : "Faculty Coordinator",
-            image : image2
+            id: 2,
+            name: "Person 2",
+            position: "Faculty Coordinator",
+            image: image2,
+            linkedin: "https://www.linkedin.com/in/person-2/"
         },
         {
-            id : 3,
-            name : "Image3",
-            position : "Faculty Coordinator",
-            image : image3
+            id: 3,
+            name: "Person 3",
+            position: "Faculty Coordinator",
+            image: image3,
+            linkedin: "https://www.linkedin.com/in/person-3/"
         },
         {
-            id : 4,
-            name : "Image4",
-            position : "Faculty Coordinator",
-            image : image4
-
+            id: 4,
+            name: "Person 4",
+            position: "Faculty Coordinator",
+            image: image4,
+            linkedin: "https://www.linkedin.com/in/person-4/"
         }
-    ]
+    ];
+
     return (
         <div>
             <div className="faculties-container">
@@ -44,23 +46,31 @@ function Faculties() {
                 </div>
 
                 <div className="faculty-coordinator-pics">
-                    {faculty_coordinators.map((value, index)=> {
-                        return(
-                            <div className='box ' key={value.id}>
-                                <img src={value.image} alt="" className='faculty-img' />
+                    {faculty_coordinators.map((value, index) => {
+                        return (
+                            <div className='box' key={value.id}>
+                                <img src={value.image} alt={value.name} className='faculty-img' />
+                                <div className="overlay">
+                                    <div className="info">
+                                        <p>{value.name}</p>
+                                        <p>{value.position}</p>
+                                        <a href={value.linkedin} target="_blank" rel="noopener noreferrer">
+                                            <span className="linkedin-icon">
+                                                <i className="fab fa-linkedin"></i>
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
-
                         )
                     })}
-
-                    
                 </div>
             </div>
             <div className="office-barriers">
-                <Officebarriers/>
+                <Officebarriers />
             </div>
         </div>
     )
 }
 
-export default Faculties
+export default Faculties;
