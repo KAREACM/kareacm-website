2e28d8ff7fe8b325398f0e745edc4711d451a9ae
Karthik Varma Chekuri <karthikvarmachekuri555@gmail.com>
2024-12-03 10:43:53 +0530

    responsive update done

--- src/new_design_components/Navbar/Navbar.jsx
+++ src/new_design_components/Navbar/Navbar.jsx
@@ -1,44 +1,55 @@
-import React from 'react'
-import './../Navbar/Navbar.css'
-import kare_acm_logo_navbar from './../../../Athithya/src/Assets/Photos/kare-acm-sc logo.png'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+import './../Navbar/Navbar.css';
+import kare_acm_logo_navbar from './../../../Athithya/src/Assets/Photos/kare-acm-sc logo.png';
+import { NavLink } from 'react-router-dom';
+
 function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <div>
             <nav>
-                <div className="header" id='header'>
-                    <img src={kare_acm_logo_navbar} className='kare_acm_logo_navbar' alt="kare_acm_logo" />
-                    <div className='acm_kare'>ACM KARE</div>
-                    <div>
-                        {/* Empty div */}
+                <div className="header" id="header">
+                    <img
+                        src={kare_acm_logo_navbar}
+                        className="kare_acm_logo_navbar"
+                        alt="kare_acm_logo"
+                    />
+                    <div className="acm_kare">ACM KARE</div>
+                    <div className="hamburger" onClick={toggleMenu}>
+                        ☰ {/* Hamburger icon */}
                     </div>
-                    <div className="navbar">
-                        <NavLink to='/' activeClassName="">
-                            <div className="home flx ">Home</div>
-                        </NavLink>
-                        <NavLink to='/about' activeClassName="">
-                            <div className="about flx">About</div>
-                        </NavLink>
-                        <NavLink to='/teams' activeClassName="">
-                        <div className="teams flx">Teams</div>
-                        </NavLink>
-                        <NavLink to='/events' activeClassName="">
-                        <div className="events flx">Events</div>
-                        </NavLink>
-                        <NavLink to='/blogs' activeClassName=''>
-                        <div className="blogs flx">Blogs</div>
-                        </NavLink>
-                        <NavLink to='/gallery' activeClassName=''>
-                        <div className="gallery flx">Gallery</div>
-                        </NavLink>
-                        <NavLink to='/contact' activeClassName=''>
-                        <div className="contact flx">Contact</div>
-                        </NavLink>
-                    </div>
+                </div>
+                <div className={`navbar ${menuOpen ? 'show' : ''}`}>
+                    <NavLink to="/" onClick={() => setMenuOpen(false)}>
+                        <div className="home">Home</div>
+                    </NavLink>
+                    <NavLink to="/about" onClick={() => setMenuOpen(false)}>
+                        <div className="about">About</div>
+                    </NavLink>
+                    <NavLink to="/teams" onClick={() => setMenuOpen(false)}>
+                        <div className="teams">Teams</div>
+                    </NavLink>
+                    <NavLink to="/events" onClick={() => setMenuOpen(false)}>
+                        <div className="events">Events</div>
+                    </NavLink>
+                    <NavLink to="/blogs" onClick={() => setMenuOpen(false)}>
+                        <div className="blogs">Blogs</div>
+                    </NavLink>
+                    <NavLink to="/gallery" onClick={() => setMenuOpen(false)}>
+                        <div className="gallery">Gallery</div>
+                    </NavLink>
+                    <NavLink to="/contact" onClick={() => setMenuOpen(false)}>
+                        <div className="contact">Contact</div>
+                    </NavLink>
                 </div>
             </nav>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
