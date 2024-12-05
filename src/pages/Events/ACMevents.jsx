5639f8ca29d995e65ed37323aa9e839876e553cb
imvsharshad <imvsharshad@gmail.com>
2024-12-05 23:48:46 +0530

    Events page updated

--- src/pages/Events/ACMevents.jsx
+++ src/pages/Events/ACMevents.jsx
@@ -29,7 +29,14 @@ import {
   techQuest,
   virtualCodeCorner,
   malathi,
-  ideathon
+  ideathon,
+  codex_series6,
+  codex_series5,
+  codex_series4,
+  codex_series3,
+  codex_series2,
+  codex_series1,
+  outreach_program,
 } from '../../constants/data';
 
 // Categorized event data
@@ -39,12 +46,12 @@ const hackathons = [
 ];
 
 const espEvents = [
-  { title: 'AI/ML Conversations', data: aiml },
+  { title: 'Convergence of Machine Learning', data: aiml },
   { title: 'ESP Blast', data: espblast },
-  { title: 'ESP in Analytics', data: esp_in_analytics },
+  { title: 'ESP ion Analytics Industry', data: esp_in_analytics },
   { title: 'ESP on IoE', data: esp_on_ioe },
   { title: 'ESP on Security', data: esp_on_security },
-  { title: 'Prashanth Event', data: prashanth },
+  { title: 'ESP on ICPC', data: prashanth },
   { title: 'ESP on Technology Trends', data: venkateshwaran}
 ];
 
@@ -69,14 +76,28 @@ const otherEvents = [
 
 const industryExpert = [
   { title: 'Fun with Algorithms', data: fun_with_algorithms },
-  { title: 'Shourya Roy Speech', data: shouryaRoy },
-  { title: 'Gen AI Webinar', data: genAiWebinar},
-  { title: 'Human Computer Interaction Webinar', data: humanComputerInteractionWebinar},
-  { title: 'Malathi talk on ideas of innovation', data: malathi}
+  { title: 'Data Science and Gen AI', data: shouryaRoy },
+  { title: 'Agentic Workflow & Gen AI', data: genAiWebinar},
+  { title: 'Human Computer Interaction', data: humanComputerInteractionWebinar},
+  { title: 'Ideas to innovation', data: malathi}
 ]
+
+const codex_series = [
+  { title: 'CodeX Series 1', data: codex_series1 },
+  { title: 'CodeX Series 2', data: codex_series2 },
+  { title: 'CodeX Series 3', data: codex_series3},
+  { title: 'CodeX Series 4', data: codex_series4},
+  { title: 'CodeX Series 5', data: codex_series5},
+  { title: 'CodeX Series 6', data: codex_series6}
+]
+
 
 const webinars = [
   { title: 'Vertex AI', data: vertexAi}
+]
+
+const out_reach_program = [
+  { title: 'Out Reach Program for Linga Global School', data: outreach_program}
 ]
 
 // Render individual event carousels
@@ -119,15 +140,15 @@ function ACMevents() {
         {/* Hackathons Section */}
         <div className="events-section">
         <h2 className="page-title section-title">Hackathons</h2>
-        <div className="event-gallery">
+        <div className="event-gallery justify-center">
           {hackathons.map((event) => renderEventCarousel(event.title, event.data))}
         </div>
       </div>
 
       {/* ideaSparks Section */}
       <div className="events-section">
-        <h2 className="page-title section-title">Idea Sparks</h2>
-        <div className="event-gallery">
+        <h2 className="page-title section-title">Idea Spark</h2>
+        <div className="event-gallery justify-center">
           {ideaSparks.map((event) => renderEventCarousel(event.title, event.data))}
         </div>
       </div>
@@ -135,7 +156,7 @@ function ACMevents() {
       {/* ideathon Section */}
       <div className="events-section">
         <h2 className="page-title section-title">Ideathon</h2>
-        <div className="event-gallery">
+        <div className="event-gallery justify-center">
           {ideathons.map((event) => renderEventCarousel(event.title, event.data))}
         </div>
       </div>
@@ -143,8 +164,25 @@ function ACMevents() {
          {/* Webinar Section */}
      <div className="events-section">
      <h2 className="page-title section-title">Webinar</h2>
-     <div className="event-gallery">
+     <div className="event-gallery justify-center">
        {webinars.map((event) => renderEventCarousel(event.title, event.data))}
+     </div>
+   </div>
+
+        {/* CodeX Series Section */}
+
+   <div className="events-section">
+     <h2 className="page-title section-title">CodeX Series</h2>
+     <div className="event-gallery ">
+       {codex_series.map((event) => renderEventCarousel(event.title, event.data))}
+     </div>
+   </div>
+
+        {/* Out Reach Program Section */}
+   <div className="events-section">
+     <h2 className="page-title section-title">Out Reach Program</h2>
+     <div className="event-gallery justify-center ">
+       {out_reach_program.map((event) => renderEventCarousel(event.title, event.data))}
      </div>
    </div>
 
