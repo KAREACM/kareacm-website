01f7093d7339984576dc7353808da0ef9ba2fa40
yash27007 <work.yashwantharavind007@gmail.com>
2025-01-06 22:30:16 +0530

    resolve merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,26 @@ import { useState } from 'react'
 import './App.css'
 import Hero from './new_design_components/Homepage/Hero-Section/Hero.jsx';
 import Navbar from './new_design_components/Navbar/Navbar.jsx';
+import About from './new_design_components/Homepage/About-Section/About.jsx';
+import { Route, Routes } from 'react-router-dom';
+import AboutACM from './new_design_components/Aboutpage/AboutACM.jsx';
+import ACMTeams from './new_design_components/Teamspage/ACMTeams.jsx';
+import ACMevents from './pages/Events/ACMevents.jsx';
+import ACMblogs from './new_design_components/Blogspage/ACMblogs.jsx';
+import ACMcontact from './new_design_components/Contactpage/ACMcontact.jsx';
 import ACMFooter from './new_design_components/Footer/ACMFooter.jsx';
-
-const Hero = lazy(() => import('./new_design_components/Homepage/Hero-Section/Hero.jsx'));
-const AboutACM = lazy(() => import('./new_design_components/Aboutpage/AboutACM.jsx'));
-const ACMTeams = lazy(() => import('./new_design_components/Teamspage/ACMTeams.jsx'));
-const ACMevents = lazy(() => import('./pages/Events/ACMevents.jsx'));
-const ACMblogs = lazy(() => import('./new_design_components/Blogspage/ACMblogs.jsx'));
-const ACMcontact = lazy(() => import('./new_design_components/Contactpage/ACMcontact.jsx'));
-const ACMGallery = lazy(() => import('./new_design_components/Gallerypage/ACMGallery.jsx'));
 import ACMGallery from './new_design_components/Gallerypage/ACMGallery.jsx';
 import { HourOfCode } from './new_design_components/Hourofcode/HourOfCode.jsx';
 
 function App() {
+
   return (
     <>
       <div>
-        <Navbar />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
+        <Navbar/>
+        <Routes>
             <Route path='/' element={<Hero />} />
             <Route path='/about' element={<AboutACM />} />
-            <Route path='/teams' element={<ACMTeams />} />
-            <Route path='/events' element={<ACMevents />} />
-            <Route path='/blogs' element={<ACMblogs />} />
-            <Route path='/contact' element={<ACMcontact />} />
-            <Route path='/gallery' element={<ACMGallery />} />
-          </Routes>
-        </Suspense>
-        <ACMFooter />
             <Route path='/teams' element={<ACMTeams/>} />
             <Route path='/events' element={<ACMevents/>}/>
             <Route path='/blogs' element={<ACMblogs/>}/>
@@ -44,8 +35,11 @@ function App() {
         <ACMFooter/>
 
       </div>
+
+
+
     </>
-  );
+  )
 }
 
-export default App;
+export default App
